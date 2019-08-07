8eed804c955f596c703fc45b45ae241be663cb31
Trumpet63-stream <53861277+Trumpet63-stream@users.noreply.github.com>
2019-08-07 06:59:48 -0400

    Create simfile_parser.js

---
+++ simfile_parser.js
@@ -0,0 +1,20 @@
+<html>
+<body>
+<input type="file" accept=".sm" id="upload" value="Upload From Computer"><br>
+<input type="button" value="Parse" onclick="go()">
+</body>
+<script>
+var reader;
+function go() {
+	let upload = document.getElementById("upload");
+	let file = upload.files[0];
+	reader = new FileReader()
+	reader.readAsText(file);
+	reader.addEventListener('loadend', printResult);
+}
+
+function printResult() {
+	reader.result.split("\n").map(e => e.trim());
+}
+</script>
+</html>
